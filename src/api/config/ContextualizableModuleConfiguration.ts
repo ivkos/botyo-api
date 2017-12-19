83e8057baa28cd606d08978d46fa0b22bab6b5dc
Ivaylo Stoyanov <me@ivkos.com>
2017-12-20 00:02:07 +0200

    ContextualizableModuleConfiguration: Add inContextOfChatThread

--- src/api/config/ContextualizableModuleConfiguration.ts
+++ src/api/config/ContextualizableModuleConfiguration.ts
@@ -1,6 +1,6 @@
 import { ModuleConfiguration } from "./ModuleConfiguration";
 import { MessageContextSwitcher } from "./MessageContextSwitcher";
-import { Message } from "../ChatApi";
+import { FacebookId, Message } from "../ChatApi";
 import SymbolUtil from "../util/SymbolUtil";
 
 /**
@@ -18,4 +18,16 @@ export abstract class ContextualizableModuleConfiguration extends ModuleConfigur
      * @since 2.0.0
      */
     abstract inContext(messageContext: Message): MessageContextSwitcher;
+
+    /**
+     * Returns the module configuration in the context of the specified chat thread ID.
+     *
+     * @param {FacebookId} chatThreadId
+     * @return {ModuleConfiguration}
+     * @since 2.0.0
+     */
+    inContextOfChatThread(chatThreadId: FacebookId): ModuleConfiguration
+    {
+        return this.inContext({ threadID: chatThreadId }).ofChatThread();
+    }
 }
