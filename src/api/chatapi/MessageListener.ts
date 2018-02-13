d71dd0e67b71dc37f6cfc86c7a0db2a444c49f4d
Ivaylo Stoyanov <me@ivkos.com>
2018-02-13 11:26:40 +0200

    Make listen() return StopListeningFunction

--- src/api/chatapi/MessageListener.ts
+++ src/api/chatapi/MessageListener.ts
@@ -8,13 +8,22 @@ import { Message } from "./ChatApiTypes";
  */
 export type MessageHandler = (err: any, msg: Message) => void;
 
+/**
+ * Function, returned by listen(), that when invoked makes the API stop listening for incoming messages and events
+ *
+ * @function
+ */
+export type StopListeningFunction = () => void;
+
 export interface MessageListener
 {
     /**
      * Sets a callback function to be called upon receiving a message that gets passed the received message.
      *
      * @param {MessageHandler} handler
+     * @return {StopListeningFunction} function that when invoked makes the API stop listening for incoming messages and
+     * events
      * @since 2.0.0
      */
-    listen(handler: MessageHandler): void;
+    listen(handler: MessageHandler): StopListeningFunction;
 }
